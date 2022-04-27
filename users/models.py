c788580212452e049070a05a909a876ef1f49e8f
12moi <12moi>
2022-04-27 14:43:02 +0300

    add employer model

--- users/models.py
+++ users/models.py
@@ -31,3 +31,37 @@ class Post(models.Model):
     description = models.TextField(max_length=255)
     file= models.CharField( )
     
+class Employer(models.Model):
+    id =  models.IntegerField(primary_key=True)
+    name=models.CharField()
+    email=models.CharField()
+    contact=models.IntegerField()
+    location= models.IntegerField(blank=True)
+    address=models.CharField()
+    company_bio=models.CharField()
+    name=models.CharField()
+    company_pic=models.Cloudinary()
+
+class Jobseeker(models.Model):
+    jobid =  models.IntegerField(primary_key=True)
+    name=models.CharField()
+    location= models.IntegerField(blank=True)
+    professsion=models.CharField()
+    jobtype=models.CharField()
+    experience=models.CharField()
+    Education_level=models.CharField()
+    job_category=models.CharField()
+    contact=models.IntegerField()
+    salary_Expectation=models.IntegerField()
+    status=models.IntegerField()
+    file=models.Cloudinary()
+    profile_pic=models.Cloudinary()
+    bio=models.CharField()
+    work=models.CharField()
+    Education=models.CharField()
+    skills=models.CharField()
+    reference=models.CharField()
+
+    
+    def save_user_move(sender, instance, **kwargs):
+        instance.move.save()  
