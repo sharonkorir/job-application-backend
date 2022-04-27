d48b0d8f8af478f913a337cb4e431ac7407d9f41
12moi <12moi>
2022-04-27 15:54:30 +0300

    add Auth_user_model to customize the user model

--- users/models.py
+++ users/models.py
@@ -8,49 +8,31 @@ from django.dispatch import receiver
 # Create your models here.
 class Jobseeker(models.Model):
     jobid =  models.IntegerField(primary_key=True)
-    name=models.CharField()
+    name=models.CharField(max_length=255)
     location= models.IntegerField(blank=True)
-    professsion=models.CharField()
-    jobtype=models.CharField()
-    experience=models.CharField()
-    Education_level=models.CharField()
-    job_category=models.CharField()
+    professsion=models.CharField(max_length=255)
+    jobtype=models.CharField(max_length=255)
+    experience=models.CharField(max_length=255)
+    Education_level=models.CharField(max_length=255)
+    job_category=models.CharField(max_length=255)
     contact=models.IntegerField()
     salary_Expectation=models.IntegerField()
     status=models.IntegerField()
-    file=models.Cloudinary()
-    profile_pic=models.Cloudinary()
-    bio=models.CharField()
-    work=models.CharField()
-    Education=models.CharField()
-    skills=models.CharField()
-    reference=models.CharField()
-
-
-
-
-class Comment(models.Model):
-    id =  models.IntegerField(primary_key=True)
-    userId=models.IntegerField()
-    content=models.CharField()
-    post=models.CharField()
-    like=models.IntegerField()
-    dislike=models.IntegerField()
-    created = models.DateTimeField(auto_now_add=True, null=True)
-
-    class Meta:
-        ordering = ["-created"]
-
-    def __str__(self):
-        return f'{self.user.name} post'
+    # file=models.Cloudinary()
+    # profile_pic=models.Cloudinary()
+    bio=models.CharField(max_length=255)
+    work=models.CharField(max_length=255)
+    Education=models.CharField(max_length=255)
+    skills=models.CharField(max_length=255)
+    reference=models.CharField(max_length=255)
 
 
 
 class Post(models.Model):
-    id =  models.IntegerField(Comment, primary_key=True)
-    title=models.CharField()
-    description=models.CharField()
-    file=models.Cloudinary()
+    id =  models.IntegerField( primary_key=True)
+    title=models.CharField(max_length=255)
+    description=models.CharField(max_length=255)
+    # file=models.Cloudinary()
 
 def __str__(self):
         return f'{self.title}'
@@ -68,24 +50,42 @@ def all_posts(cls):
 
 def save_post(self):
         self.save()
+class Comment(models.Model):
+    id =  models.IntegerField(primary_key=True ) 
+    userId=models.IntegerField()
+    content=models.CharField(max_length=255)
+    post=models.CharField(max_length=255)
+    like=models.IntegerField()
+    dislike=models.IntegerField()
+    created = models.DateTimeField(auto_now_add=True, null=True)
+
+    class Meta:
+        ordering = ["-created"]
+
+    def __str__(self):
+        return f'{self.user.name} post'
+
+
+
+
 
 class Employer(models.Model):
     id =  models.IntegerField(Post, primary_key=True)
-    name=models.CharField()
-    email=models.CharField()
+    name=models.CharField(max_length=255)
+    email=models.CharField(max_length=255)
     contact=models.IntegerField()
     location= models.IntegerField(blank=True)
-    address=models.CharField()
-    company_bio=models.CharField()
-    name=models.CharField()
-    company_pic=models.Cloudinary()
+    address=models.CharField(max_length=255)
+    company_bio=models.CharField(max_length=255)
+    name=models.CharField(max_length=255)
+    # company_pic=models.Cloudinary()
 
 class User(AbstractUser):
     username = models.CharField(max_length=100, unique=True)
     password = models.CharField(max_length=255)
     email = models.EmailField(max_length=100)
-    is_jobseeker = models.ForeignKey(Jobseeker, related_name='user')
-    is_employer = models.ForeignKey(Employer, related_name='user')
+    is_jobseeker = models.ForeignKey(Jobseeker, on_delete=models.CASCADE, related_name='user')
+    is_employer = models.ForeignKey(Employer, on_delete=models.CASCADE, related_name='user')
 
     REQUIRED = []
 
