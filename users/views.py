991295022d18679207fcd6a93f562adbcbe9c73f
12moi <12moi>
2022-04-28 12:17:14 +0300

    add profile method

--- users/views.py
+++ users/views.py
@@ -23,3 +23,16 @@ def signup(request):
     else:
         form = SignupForm()
     return render(request, '', {'form': form})
+
+@login_required(login_url='login')
+def profile(request, username):
+    return render(request, 'profile.html')
+
+def user_profile(request, username):
+    user_prof = get_object_or_404(User, username=username)
+    if request.user == user_prof:
+        return redirect('profile', username=request.user.username)
+    params = {
+        'user_prof': user_prof,
+    }
+    return render(request, '', params)
