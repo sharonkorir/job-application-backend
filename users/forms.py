5286db25814d305a6b57ff9b2e4a5abe3d9c8673
12moi <12moi>
2022-04-28 10:28:25 +0300

    add forms

---
+++ users/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import  Comment, Jobseeker, Employer,Post
+
+
+
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('name', 'email', 'Phone no', 'Address','password','confirmpassword')
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ['user']
+
+class EmployeeForm(forms.ModelForm):
+    class Meta:
+        model = Employer
+        exclude = ['user']
+
+class JobseekerForm(forms.ModelForm):
+    class Meta:
+        model = Jobseeker
+        exclude = ('user', 'bio','Education', 'Work_experience','skills','References','avalaibility','salary_expections','Job_category')
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        exclude = ['user']
