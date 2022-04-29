b737b0d4cc1137ede949e5394f37a31ce5dc6ed3
12moi <12moi>
2022-04-29 15:51:01 +0300

    register models

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.Profile)
+admin.site.register(models.Post)
+admin.site.register(models.Job)
+admin.site.register(models.Jobseeker)
--- users/urls.py
+++ users/urls.py
@@ -12,7 +12,7 @@ from .views import  MpesaPaymentViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-# router.register('MpesaPayment', views.MpesaPaymentViewSet)
+router.register('MpesaPayment', views.MpesaPaymentViewSet)
 router.register('Job', views.JobViewSet)
 # router.register('posts', views.PostViewSet)
 # router.register('profile', views.ProfileViewSet)
@@ -23,10 +23,9 @@ urlpatterns = [
      path('', include(router.urls)),
      path('api/', include(router.urls)),
      path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+     path('MpesaPayment/', views.Job, name='MpesaPayment'),
     path('job/', views.Job, name='Job'),
-    # path('account/', include('django.contrib.auth.urls')),
-    # path('profile/<username>', views.profile, name='profile'),
-    # path('profile/<username>/edit/', views.edit_profile, name='edit-profile'),
+   
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
