654bb40cf26f6ce4b08b7dbfa52a01bc1e5b1b12
sharonkorir <sharon.korir@student.moringaschool.com>
2022-05-05 00:15:37 +0300

    add generate access token function

--- mpesa_api/views.py
+++ mpesa_api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import requests
+from requests.auth import HTTPBasicAuth
+import json
 
 # Create your views here.
+
+def getAccessToken(request):
+    consumer_key = 'zXew3BUDVwbHDuRC5xXFACWXq5PnhOUf'
+    consumer_secret = 'O4VufZDDQb5R7Iib'
+    api_URL = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
+    r = requests.get(api_URL, auth=HTTPBasicAuth(consumer_key, consumer_secret))
+    mpesa_access_token = json.loads(r.text)
+    validated_mpesa_access_token = mpesa_access_token['access_token']
+    return HttpResponse(validated_mpesa_access_token)
--- worklinks/settings.py
+++ worklinks/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
+    'mpesa_api',
     
 ]
 
