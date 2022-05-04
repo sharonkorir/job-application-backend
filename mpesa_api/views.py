1c90dd06311832789a72d1493e8b417c1a01f847
sharonkorir <sharon.korir@student.moringaschool.com>
2022-05-05 00:21:19 +0300

    add function to initiate mpesa STK push

--- mpesa_api/views.py
+++ mpesa_api/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 import requests
 from requests.auth import HTTPBasicAuth
 import json
+from . mpesa_credentials import MpesaAccessToken, LipanaMpesaPpassword
 
 # Create your views here.
 
@@ -14,3 +15,24 @@ def getAccessToken(request):
     mpesa_access_token = json.loads(r.text)
     validated_mpesa_access_token = mpesa_access_token['access_token']
     return HttpResponse(validated_mpesa_access_token)
+
+def lipa_na_mpesa_online(request):
+    access_token = MpesaAccessToken.validated_mpesa_access_token
+    api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+    headers = {"Authorization": "Bearer %s" % access_token}
+    request = {
+        "BusinessShortCode": LipanaMpesaPpassword.Business_short_code,
+        "Password": LipanaMpesaPpassword.decode_password,
+        "Timestamp": LipanaMpesaPpassword.lipa_time,
+        "TransactionType": "CustomerPayBillOnline",
+        "Amount": 1,
+        "PartyA": 254702855081,  # replace with your phone number to get stk push
+        "PartyB": LipanaMpesaPpassword.Business_short_code,
+        "PhoneNumber": 254702855081,  # replace with your phone number to get stk push
+        "CallBackURL": "https://sandbox.safaricom.co.ke/mpesa/",
+        "AccountReference": "Henry",
+        "TransactionDesc": "Testing stk push"
+    }
+    #redircet user to homepage after successful payment
+    response = requests.post(api_url, json=request, headers=headers)
+    return HttpResponse('success')
