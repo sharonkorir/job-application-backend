bf09534c24065b39028f65de216578997119411f
12moi <moi.shadrack@student.moringaschool.com>
2022-05-05 11:54:32 +0300

    add database

--- worklinks/settings.py
+++ worklinks/settings.py
@@ -141,12 +141,12 @@ CORS_ALLOW_METHODS = [
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # REST_FRAMEWORK = {
 #     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',)
